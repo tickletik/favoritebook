447bed23df6d8a202a553dbffede420145f6df93
Some name <ronny.coder @gmail.com>
2017-12-05 15:52:07 +0200

    hooked up meatballs to textfields

--- FavoriteBook/BookFormTableVC.swift
+++ FavoriteBook/BookFormTableVC.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class BookFormTableVC: UITableViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var genreTextField: UITextField!
+    @IBOutlet weak var lengthTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
